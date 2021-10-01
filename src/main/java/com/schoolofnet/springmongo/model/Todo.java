d8bd1d58de9528c9fa2b5dac2d85cb3e637eb5fd
SamuWesll <samuelwrochabarboza@gmail.com>
2021-10-01 11:44:56 -0300

    aula - Otimitizando com lombok

---
+++ src/main/java/com/schoolofnet/springmongo/model/Todo.java
@@ -0,0 +1,20 @@
+package com.schoolofnet.springmongo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "todos")
+public class Todo {
+
+    @Id
+    private String id;
+
+    private String title;
+
+}
